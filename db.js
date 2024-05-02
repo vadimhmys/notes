3f1f93c77c02b1223cf8c6ecba6b15759c7294eb
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-02 21:06:19 +0300

    kjchalkhc

--- db.js
+++ db.js
@@ -1,11 +1,3 @@
 import {Sequelize} from 'sequelize'
 
-export default new Sequelize(
-    process.env.DB_NAME, // база данных
-    process.env.DB_USER, // пользователь
-    process.env.DB_PASS, // пароль
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST
-    }
-)
+export default new Sequelize(process.env.DATABASE_URL);
