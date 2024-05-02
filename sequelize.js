120e2b1d3705ee18cfdfafd926d041009274cb39
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-02 19:42:02 +0300

    kjdshs

--- sequelize.js
+++ sequelize.js
@@ -6,7 +6,6 @@ export default new Sequelize(
     process.env.DB_PASS, // пароль
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        host: process.env.DB_HOST
     }
 )
