9c19a01a15e52ef4b102d2c9ca3be5cd222e9b3a
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-02 23:56:28 +0300

    dedewde

--- index.js
+++ index.js
@@ -5,25 +5,8 @@ import * as mapping from './models/mapping.js';
 import cors from 'cors';
 import router from './routes/index.js';
 import ErrorHandler from './middleware/ErrorHandler.js';
-//import { Client } from 'pg';
-/* import pkg from 'pg';
-const { Client } = pkg; */
 
 const PORT = process.env.PORT || 5000;
-/* 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-}); */
 
 const app = express();
 app.use(cors());
