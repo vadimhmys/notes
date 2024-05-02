1ea9c64fba89feee35c1fbaa82d26efcbf9cd8f4
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-03 00:05:20 +0300

    cdefwefwsa

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
-import {Sequelize} from 'sequelize'
+import { Sequelize } from 'sequelize';
 
-export default new Sequelize(process.env.DATABASE_URL, {
+let sequelize;
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialectOptions: {
       ssl: {
         require: true,
@@ -8,3 +11,16 @@ export default new Sequelize(process.env.DATABASE_URL, {
       },
     },
   });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            dialect: 'postgres',
+            host: process.env.DB_HOST
+        }
+    );
+}
+
+export default sequelize;
