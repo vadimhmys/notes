70086661aaef2618e68213a823d79d338fcc92a8
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-02 22:08:34 +0300

    lkjsl

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ import * as mapping from './models/mapping.js';
 import cors from 'cors';
 import router from './routes/index.js';
 import ErrorHandler from './middleware/ErrorHandler.js';
+import { Client } from 'pg';
 
 const PORT = process.env.PORT || 5000;
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const app = express();
 app.use(cors());
