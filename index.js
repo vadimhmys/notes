917f2099f9f118548f3d04a110703cbd415dcbe3
Vadim Hmys <vadimhmyz@gmail.com>
2024-05-02 21:37:04 +0300

    kjdhfskn

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/', (req, res) => {
+  res.send("Hello, world!");
+});
+
 app.use('/api', router);
 
 app.use(ErrorHandler);
